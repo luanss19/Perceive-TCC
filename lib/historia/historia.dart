5f0a492bf3938bb8be7fda155ef9856ad8e2d14b
luanss19 <magestadeluanss@gmail.com>
2021-06-15 01:15:06 -0300

    Adição da parte lógica das escolhas do usuário

--- lib/historia/historia.dart
+++ lib/historia/historia.dart
@@ -2,12 +2,16 @@ class Historia {
   String _tituloHistoria;
   String _escolha1;
   String _escolha2;
+  String _escolha3;
 
-  Historia({String tituloHistoria, String escolha1, String escolha2}){
+  Historia({String tituloHistoria, String escolha1, String escolha2, String _escolha3}){
     this._tituloHistoria = tituloHistoria;
     this._escolha1 = escolha1;
     this._escolha2 = escolha2;
+    this._escolha3 = escolha3;
   }
+
+  String get escolha3 => _escolha3;
 
   String get escolha2 => _escolha2;
 
